07e0508ab8bd4fb79635bb9b243fdfaa3cd653ba
micaldas <micaldas@mailfence.com>
2021-08-07 23:39:34 +0100

    Nth Commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.9
 """ Automation of the git process of adding, commiting and pushing data """
 import subprocess
 
@@ -8,8 +8,8 @@ def push():
     title = input('Choose a title for the commit ')
     subprocess.run(['git add .'], shell=True)
     subprocess.run(['git commit -m ' + title], shell=True)
-    subprocess.run(['git push origin main'], shell=True)
-    subprocess.run(['git push origin_gogs main'], shell=True)
+    subprocess.run(['git push origin master'], shell=True)
+    subprocess.run(['git push origin_gogs master'], shell=True)
 
 
 if __name__ == '__main__':
